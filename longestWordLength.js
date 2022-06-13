591be8bf3b600066eebfae734262afd58d8b1117
Mostafa Milani Amin <90893927+Mostafa-Milani-Amin@users.noreply.github.com>
2022-06-13 17:56:07 +0430

    Add ".replace(/[\u2000-\u206F..."
    Ho aggiunto ".replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"()*+,\-.\/:;<=>?\[\]_`{|}]/g,"")" per escludere i segni di interpunzione dal calcolo.

--- longestWordLength.js
+++ longestWordLength.js
@@ -1,1 +1,1 @@
-const longestWordLength = str => str.split(' ').sort((a, b) => b.length - a.length)[0].length;
+const longestWordLength = str => str.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"()*+,\-.\/:;<=>?\[\]_`{|}]/g,"").split(' ').sort((a, b) => b.length - a.length)[0].length;
