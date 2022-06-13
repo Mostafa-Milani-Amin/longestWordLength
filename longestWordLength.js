c058a6943f072c26158be10afaefaa0c95742c16
Mostafa Milani Amin <90893927+Mostafa-Milani-Amin@users.noreply.github.com>
2022-06-13 20:16:47 +0430

    "split(' ')" ----> "trim().split(/\s+/)"
    Risolto il bug che dava split(' ') creando array vuoti e falsando il risultato quando nella stringa c'erano più spazi consecutivi.

--- longestWordLength.js
+++ longestWordLength.js
@@ -1,1 +1,1 @@
-const longestWordLength = str => str.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"()*+,\-.\/:;<=>?\[\]_`{|}]/g,"").split(' ').sort((a, b) => b.length - a.length)[0].length;
+const longestWordLength = str => str.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"()*+,\-.\/:;<=>?\[\]_`{|}]/g,"").trim().split(/\s+/).sort((a, b) => b.length - a.length)[0].length;
